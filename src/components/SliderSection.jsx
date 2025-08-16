002e6db253f9b4771d45f5a4003a46b064732dd9
akshay-ken <akshaybest3120@gmail.com>
2025-08-16 15:38:15 +0530

    style: add bg and padding

--- src/components/SliderSection.jsx
+++ src/components/SliderSection.jsx
@@ -1,6 +1,6 @@
 export function SliderSection() {
   return (
-    <section className="w-sm md:w-xl">
+    <section className="w-sm bg-Blue-850 p-6 md:w-xl">
       <p>You’ve used 815 GB of your storage</p>
     </section>
   );

6ce57b4c23de551f4eeb8ae32d6652cb7f391071
akshay-ken <akshaybest3120@gmail.com>
2025-08-16 15:38:46 +0530

    style: add flex row for desktop

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { SliderSection } from "./components/SliderSection";
 function App() {
   return (
     <>
-      <main className="flex h-screen flex-col items-center justify-center gap-4 md:gap-8">
+      <main className="flex h-screen flex-col items-center justify-center gap-4 md:flex-row md:gap-8">
         <HeaderSection />
         <SliderSection />
       </main>

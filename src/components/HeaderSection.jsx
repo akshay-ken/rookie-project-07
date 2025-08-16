95c9d50896a54f15fdb11ad3500781d7ca9ec480
akshay-ken <akshaybest3120@gmail.com>
2025-08-16 15:19:12 +0530

    style: add border radius to jsx code

--- src/components/HeaderSection.jsx
+++ src/components/HeaderSection.jsx
@@ -2,7 +2,7 @@ import flyoIcon from "../assets/images/logo.svg";
 
 export function HeaderSection() {
   return (
-    <header className="w-sm bg-Blue-850 p-6">
+    <header className="w-sm rounded-[0.8rem] rounded-tr-[5rem] bg-Blue-850 p-6">
       <img src={flyoIcon} alt="" />
       <h1></h1>
     </header>

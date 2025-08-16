7b913d399f0c5cd3e68e57a37851780977f1deeb
akshay-ken <akshaybest3120@gmail.com>
2025-08-16 17:49:51 +0530

    style: add p tag with styles

--- src/components/SliderSection.jsx
+++ src/components/SliderSection.jsx
@@ -1,7 +1,11 @@
 export function SliderSection() {
   return (
-    <section className="w-sm bg-Blue-850 p-6 md:w-xl">
-      <p>You’ve used 815 GB of your storage</p>
+    <section className="w-sm rounded-2xl bg-Blue-850 p-6 md:w-xl">
+      <p className="text-base text-white/80">
+        You’ve used <span className="font-semibold text-white"> 815 GB </span>{" "}
+        of your storage
+      </p>
+      <div></div>
     </section>
   );
 }

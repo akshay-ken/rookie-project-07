44be25144bee9db0f64a35ff434060886dfba473
akshay-ken <akshaybest3120@gmail.com>
2025-08-16 15:35:13 +0530

    feat: add slider component and use it in app.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { HeaderSection } from "./components/HeaderSection";
+import { SliderSection } from "./components/SliderSection";
 
 function App() {
   return (
     <>
       <main className="flex h-screen flex-col items-center justify-center gap-4 md:gap-8">
         <HeaderSection />
+        <SliderSection />
       </main>
     </>
   );

33843d97943c3508c7c74f551b953a88a300a85a
akshay-ken <akshaybest3120@gmail.com>
2025-08-16 15:05:45 +0530

    chore: import img logo for header and use it in component

--- src/components/HeaderSection.jsx
+++ src/components/HeaderSection.jsx
@@ -1,6 +1,9 @@
+import flyoIcon from "../assets/images/logo.svg";
+
 export function HeaderSection() {
   return (
     <header className="w-sm bg-Blue-850 p-6">
+      <img src={flyoIcon} alt="" />
       <h1></h1>
     </header>
   );
